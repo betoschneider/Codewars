8487f2b2e6bb82bd30bfffdac7eba54839f389b0
betoschneider <roberto.schneider@gmail.com>
2022-08-18 14:54:31 -0300

    nova solucao com list comprehension

--- Multiples_of_3_or_5.py
+++ Multiples_of_3_or_5.py
@@ -8,18 +8,8 @@ Created on Thu Aug 18 07:38:42 2022
 #link do desafio: https://www.codewars.com/kata/514b92a657cdc65150000006/python
 
 def solution(number):
-    if number < 0:
-        return 0
-    mult = []
-    for i in range(number):
-        if i % 3 == 0 or i % 5 == 0:
-            mult.append(i)
-    soma = 0
-    for i in mult:
-        soma += i
-    return soma
-
+    return sum([x for x in range(number) if x % 3 == 0 or x % 5 == 0])
 
 if __name__ == '__main__':
-    teste = solution(-10)
+    teste = solution(10)
     print(teste)
