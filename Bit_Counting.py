bb3253d354d551bcbe28dafdb3df5f627189767d
betoschneider <roberto.schneider@gmail.com>
2022-08-19 17:38:56 -0300

    novo desafio + conversor b10/b2

---
+++ Bit_Counting.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 19 17:21:05 2022
+
+@author: Roberto Schneider
+"""
+
+#solução do desafio
+def count_bits(n):
+    b = 0
+    while n >= 2:
+        b += n % 2
+        n = n // 2
+    b += n
+    return b
+print(count_bits(1234))
+
+
+#converte um inteiro da base 10 para binário
+def convert_bin(n):
+    b = ''
+    while n >= 2:
+        b += str(n % 2)
+        n = n // 2
+    b += str(n)
+    return b[::-1]
+print(convert_bin(13))
